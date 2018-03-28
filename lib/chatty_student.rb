a7d4fb706c669b35ab046c34b478f5dbfde9c598
Holden Altaffer <haltaffer@gmail.com>
2018-03-28 18:24:41 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,7 +6,16 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-    10.times do |raise_hand|
-    end
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+
+
   end
 end
